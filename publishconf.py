5bb5ecfadf509bb86e84474f8ed989b29fc84340
itech001 <itech001@126.com>
2016-06-27 12:54:53 +0800

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.ciandcd.com'
+SITEURL = 'http://ciandcd.github.io/ciandcd-web'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
